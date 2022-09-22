6afa3c895e4b9e8e5ab1cdfabb48bb0cd6c14833
Troelsmou <107362995+Troelsmou@users.noreply.github.com>
2022-09-22 13:48:06 +0200

    Add files via upload

--- src/dft.py
+++ src/dft.py
@@ -3,12 +3,26 @@
 from typing import Iterable
 from tree import T
 
+def in_order(t: T or None) -> Iterable[int]:
+    output_list = list()
+    stack = list()
+    current_node = t
+    while True:
+        if current_node != None:
+            stack.append(current_node)
+            current_node = current_node.left
+        elif stack:
+            popped = stack.pop()
+            output_list.append(popped.val)
+            current_node = popped.right
+        else:
+            break
 
-def in_order(t: T | None) -> Iterable[int]:
-    """In-order traversal of a tree.
+    return output_list
 
-    >>> tree = T(2, T(1, None, None), T(4, T(3, None, None), T(5, None, None)))
-    >>> list(in_order(tree))
-    [1, 2, 3, 4, 5]
-    """
-    return  # FIXME
+
+
+tree = T(2, T(1, None, None), 
+    T(4, T(3, None, None), T(5, None, None)))
+
+print(in_order(tree))
