0f822705ce933388a7e7ed78911111f36ca977e0
Troelsmou <107362995+Troelsmou@users.noreply.github.com>
2022-09-24 14:50:13 +0200

    Add files via upload

--- src/bft.py
+++ src/bft.py
@@ -9,17 +9,19 @@ def bf_order(t: T or None) -> Iterable[int]:
     if t == None:
         return []
 
-    node_queue = [t]
+    node_queue = deque([t])
     output_list = []
 
     while node_queue:
         if isinstance(node_queue[0], T):
-            current_node = node_queue.pop(0)
+            current_node = node_queue.popleft()
             output_list.append(current_node.val)
             node_queue.append(current_node.left)
             node_queue.append(current_node.right)
         else:
-            node_queue.pop(0)
-    return output_list # FIXME
+            node_queue.popleft()
+    return output_list
 
-list(bf_order(None))
+tree = T(2, T(1, None, None), T(4, T(3, None, None), T(5, None, None)))
+
+print(bf_order(tree))
